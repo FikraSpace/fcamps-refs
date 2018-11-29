c73be2f68e67e6c3349dab83da5ed2cde495e9eb
Ali M. Ismail <aismail@solostudio.co>
2018-11-29 20:59:43 +0300

    2nd commit

--- app.js
+++ app.js
@@ -3,26 +3,67 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
+    this.state = {
+      inputValue: 120
+    }
     this.canvas = React.createRef()
+    
   }
 
   componentDidMount() {
-    let ctx = this.canvas.current.getContext('2d')
-    ctx.lineWidth = 30
-    ctx.strokeStyle = '#f00'
-    ctx.rect(100, 100, 100, 100)
-    ctx.stroke()
+    this.drawTree(this.state.inputValue)
+  }
+
+  componentDidUpdate() {
+    this.canvas.current.getContext('2d').clearRect(0,0,1000,600)
+    this.drawTree(this.state.inputValue)
+  }
+
+  drawTree(len) {
+    let ctx = this.canvas.current.getContext("2d");
+    function draw(startX, startY, len, angle) {
+      ctx.beginPath();
+      ctx.save();
+
+      ctx.translate(startX, startY);
+      ctx.rotate(angle * Math.PI / 180);
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, -len);
+      ctx.stroke();
+
+      if (len < 10) {
+        ctx.restore();
+        return;
+      }
+
+      draw(0, -len, len * 0.8, -15);
+      draw(0, -len, len * 0.8, 15);
+
+      ctx.restore();
+
+    }
+
+    draw(350, 600, len, 0);
+
+  }
+
+
+  onChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
-        <canvas width="1000" height="600" ref={this.canvas}></canvas>
+        <input type="number" value={this.state.inputValue} onChange={this.onChange.bind(this)}/>
+        <canvas width="1000px" height="600px" ref={this.canvas} />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
